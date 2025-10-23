90ec53e7b47f8c5f1ee6263c6de6da7217ad76fb
mokwelekutlwano21-lab <mokwelekutlwano21@gmail.com>
2025-10-23 08:27:08 +0200

    Updated the Movies/Index.cshtml.cs page OnGetAsync method

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace RazorPagesMovie.Models
+namespace RazorPagesMovie.Models;
+
+public class Movie
 {
-    public class Movie
-    {
-        public int Id { get; set; }
-        public string? Title { get; set; }
-        [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
-        public decimal Price { get; set; }
-        }
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
 
-        }
+    [Display(Name = "Release Date")]
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate { get; set; }
+    public string Genre { get; set; } = string.Empty;
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+}
